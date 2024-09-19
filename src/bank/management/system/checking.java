bf1d60e9d4be50079ee9908135656eddbc7a3cf1
Amish929 <agarwalamish624@gmail.com>
2024-09-19 09:57:13 +0530

    o

---
+++ src/bank/management/system/checking.java
@@ -0,0 +1,16 @@
+
+package bank.management.system;
+
+import javax.swing.*;
+public class checking extends JFrame{
+    checking(){
+        setSize(800,480);
+        setVisible(true);
+        setLocation(350,200);
+    }
+    public static void main(String args[]){
+        new checking();
+    }
+    
+    
+}
